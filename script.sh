cb17b0523d26dde69a7e71a57d980300ab735b69
Charles Maison-Blanche <Charles@macbook-pro-de-charles.home>
2020-04-15 19:01:01 +0200

    MaJ 15/05/2020 - 19:00

--- script.sh
+++ script.sh
@@ -7,6 +7,18 @@ sed -i '' -e 's/96b/927/g' ./assets/gallery/style.css
 sed -i '' -e 's/<p class="mbr-author-name pt-4 mb-2 mbr-fonts-style display-7">&nbsp;<\/p>//g' ./index.html
 sed -i '' -e 's/<p class="mbr-author-desc mbr-fonts-style display-7">&nbsp;<\/p>//g' ./index.html
 
+# On supprime les commentaires inutiles
+sed -i '' -e '/Site made with Mobirise Website Builder/d' ./index.html
+sed -i '' -e '/Site made with Mobirise Website Builder/d' ./404.html
+sed -i '' -e '/<meta name="generator"/d' ./index.html
+sed -i '' -e '/<meta name="generator"/d' ./404.html
+sed -i '' -e 's/<section class="engine".*<\/a><\/section>//g' ./index.html
+sed -i '' -e 's/<section class="engine".*<\/a><\/section>//g' ./404.html
+
+# On supprime les lignes vides
+sed -i '' -e '/^ *$/d' ./index.html
+sed -i '' -e '/^ *$/d' ./404.html
+
 # Mise en forme du titre et ajoute d'un rectangle blanc sur la page index.html
 sed -i '' -e 's/<div class="mbr-white col-md-10">/<div class="mbr-white col-md-4">/g' ./index.html
 sed -i '' -e 's/<h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-5">/<h1 class="mbr-section-title mbr-bold pb-1 mbr-fonts-style display-5" style="height: auto; background-color:white;width: auto;margin: auto;border-style: solid; font-size: 1.6em;">/g' ./index.html
