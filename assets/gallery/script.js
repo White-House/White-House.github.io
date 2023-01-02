b8bc03242fdba5da3aa291c9a38bf5d3af95997f
Charles <charlesmaisonb@yahoo.Fr>
2023-01-02 19:25:59 +0100

    MaJ 02/01/23 - 19:25

--- assets/gallery/script.js
+++ assets/gallery/script.js
@@ -20,7 +20,7 @@
     if (!isBuilder) {
         /* google iframe */
         var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
+        /* tag.src = "https://www.youtube.com/iframe_api"; */
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         var players = [];
