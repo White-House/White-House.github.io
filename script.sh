f2bb5b43bdbe0f3d8e2951ddb7e14a630b709579
Charles <charlesmaisonb@yahoo.Fr>
2023-01-08 20:14:11 +0100

    MaJ 08/01/23 - 20:10

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # On remplace le chemin des fichiers en relative depuis la racine
-sed -i '' -e 's/href="assets/href="\/assets/g' *.html
-sed -i '' -e 's/src="assets/src="\/assets/g' *.html
+sed -i '' -e 's/href="assets/href=".\/assets/g' *.html
+sed -i '' -e 's/src="assets/src=".\/assets/g' *.html
 
 # On ajoute les effets a l'image principale (snow, bubble)
-sed -i '' -e 's/href="\/assets\/gallery\/style.css">/href="\/assets\/gallery\/style.css"><link rel="stylesheet" href="\/assets\/snow\/snow.css"><link rel="stylesheet" href="\/assets\/bubble\/bubble.css">/g' ./index.html
-sed -i '' -e 's/mbr-fullscreen" id="header2-1">/mbr-fullscreen" id="header2-1"><iframe style="position: absolute; width: 100%; height: 100%; top: 0; border: none;" scrolling="no" id="animation-wallpaper" src="\/assets\/snow\/snow.html"><\/iframe>/g' ./index.html
+sed -i '' -e 's/href="\/assets\/gallery\/style.css">/href=".\/assets\/gallery\/style.css"><link rel="stylesheet" href=".\/assets\/snow\/snow.css"><link rel="stylesheet" href=".\/assets\/bubble\/bubble.css">/g' ./index.html
+sed -i '' -e 's/mbr-fullscreen" id="header2-1">/mbr-fullscreen" id="header2-1"><iframe style="position: absolute; width: 100%; height: 100%; top: 0; border: none;" scrolling="no" id="animation-wallpaper" src=".\/assets\/snow\/snow.html"><\/iframe>/g' ./index.html
 
 # On modifie le type de flèche sur la page principale
 sed -i '' -e 's/<i class="mbri-down mbr-iconfont"><\/i>/<i class="mbri-arrow-down mbr-iconfont" style="top:-5px"><\/i><i class="mbri-arrow-down mbr-iconfont" style="top:5px"><\/i>/g' ./index.html
@@ -19,7 +19,7 @@ sed -i '' -e 's/96b/927/g' ./assets/gallery/style.css
 sed -i '' -e 's/background-position: 50% 50%;/background-position: 85% 50%;/g' ./assets/theme/css/style.css 
 
 # On modifie la taille de l'image en pleine page pour une meilleure résolution 'col-lg-12'
-sed -i '' -e 's/assets\/images\/photo-2022-10-03-15-33-38-2000x1125-800x450/assets\/images\/photo-2022-10-03-15-33-38-2000x1125/g' ./index.html
+sed -i '' -e 's/assets\/images\/photo-2022-10-03-15-33-38-2000x1125-800x450/.assets\/images\/photo-2022-10-03-15-33-38-2000x1125/g' ./index.html
 
 # On modifie l'espacement des images
 sed -i '' -e 's/p-1{padding:.25rem!important}/p-1{padding:.12rem!important}/g' ./assets/bootstrap/css/bootstrap-grid.min.css 
@@ -43,8 +43,8 @@ sed -i '' -e 's/<section class="engine".*<\/a><\/section>//g' ./404.html
 sed -i '' -e 's/if(!document.getElementById("top-1")).*)}//g' ./assets/theme/js/script.js
 
 # On supprime les scripts inutiles
-sed -i '' -e 's/<script src="\/assets\/sociallikes\/social-likes.js"><\/script>//g' ./index.html
-sed -i '' -e 's/<script src="\/assets\/vimeoplayer\/jquery.mb.vimeo_player.js"><\/script>//g' ./index.html
+sed -i '' -e 's/<script src=".\/assets\/sociallikes\/social-likes.js"><\/script>//g' ./index.html
+sed -i '' -e 's/<script src=".\/assets\/vimeoplayer\/jquery.mb.vimeo_player.js"><\/script>//g' ./index.html
 
 # On supprime l'appel youtube dans script.js pour gagner du temps de chargement
 sed -i '' -e 's/tag.src = "https:\/\/www.youtube.com\/iframe_api";/\/*tag.src = "https:\/\/www.youtube.com\/iframe_api";*\//g' ./assets/gallery/script.js
@@ -65,7 +65,7 @@ sed -i '' -e 's/<span class="icon-focus"><\/span>//g' ./index.html
 sed -i '' -e 's/<span class="mbr-gallery-title mbr-fonts-style display-7">/<\/div><span class="button-info-image image-info display-7"><i class="fa fa-info-circle" aria-hidden="true"><\/i>\&ensp;/g' ./index.html
 sed -i '' -e 's/<\/span><\/div><\/div>/<\/span><\/div>/g' ./index.html
 
-# Insert source and code for easy loader
+# On ajoute le code pour le easy loader de chargement de la page
 sed -i '' -e '/<body>/a\ 
 <div id="loader-wrapper"><div id="loader"><\/div><div class="loader-section section-left"><\/div><div class="loader-section section-right"><\/div><\/div>' ./index.html
 sed -i '' -e '/<\/title>/a\ 
@@ -79,10 +79,37 @@ sed -i '' -e '/<\/body>/i\
 sed -i '' -e 's/<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top collapsed bg-color transparent">/<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top collapsed bg-color transparent" id="navbar">/g' ./index.html
 sed -i '' -e 's/<div class="navbar-brand">/<div class="navbar-brand"><input class="input-night-button" type="checkbox" id="switch-button" onclick="dayOrNightSelector()"><label class="night-button" for="switch-button"><i class="fas fa-sun"><\/i><i class="fas fa-moon"><\/i><\/label>/g' ./index.html
 sed -i '' -e '/<\/body>/i\
-<script src="/assets/navbar-mode/navbar-mode.js"></script>' ./index.html
+<script src="./assets/navbar-mode/navbar-mode.js"></script>' ./index.html
 sed -i '' -e '/<\/title>/i\
-<link rel="stylesheet" href="/assets/fa/css/all.css">' ./index.html
+<link rel="stylesheet" href="./assets/fa/css/all.css">' ./index.html
 sed -i '' -e 's/text-black/text-white text-black/g' ./index.html
 
 # Désactivation du clique droit souris (pour ne pas enregistrer les photos simplement)
-sed -i '' -e 's/<body>/<body oncontextmenu="return false">/g' ./index.html
+sed -i '' -e 's/<body>/<body onmousedown="return false" onselectstart="return false" oncontextmenu="return false">/g' ./index.html
+
+# On ajoute un retour à la ligne pour meilleur lecture du texte (et ensuite )
+sed -i '' -e $'s/<\/div>/\\\n<\/div>/g' ./index.html
+
+# On modifie la balise Titre de toutes les images (principalement pour indexation google)
+STR='alt="';
+newTitle="";
+lineNumber=1; 
+
+cat ./index.html | while read line; do 
+	# Si la ligne contient une balise alt 
+	if [[ "$line" =~ .*"$STR".* ]]; then 
+		# Si la balise alt est vide, on remplace la balise alt et title par des valeurs par défaut
+		if [[ `echo $line | egrep -io 'alt=""'` ]]; then
+			sed -i '' -e "${lineNumber}s/alt=\"\"/alt=\"Benoit Maison-Blanche | Photographe \& Explorateur\"/" ./index.html; 
+			sed -i '' -e "${lineNumber}s/title=\"\">/title=\"Benoit Maison-Blanche | Photographe \& Explorateur\">/" ./index.html;
+		else 
+		# Sinon on récupére la balise alt et on l'ajoute a la balise title
+			newTitle=$(echo $line | egrep -io "alt=\".*\" title" | egrep -io "\".*\"" | cut -d '"' -f 2);
+			sed -i '' -e "${lineNumber}s!title=\"\"!title=\"${newTitle}\"!" ./index.html;
+		fi;
+	fi; 
+	lineNumber=$((lineNumber+1));
+done
+
+# Au cas ou des images n'aient pas encore une description (localisation), on remplace le texte par défaut
+sed -i '' -e 's/Tapez la légende ici/LOCALISATION EN COURS .../g' ./index.html
